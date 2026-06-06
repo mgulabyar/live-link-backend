4a75dd009370f4b229c906795ea980cca5aa7d71
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-06-06 07:57:39 +0500

    add one more logic

--- src/models/Link.js
+++ src/models/Link.js
@@ -1,3 +1,43 @@
+// const mongoose = require("mongoose");
+
+// const LinkSchema = new mongoose.Schema({
+//   linkId: { 
+//     type: String, 
+//     required: true, 
+//     unique: true 
+//   },
+//   excelFileId: { 
+//     type: String, 
+//     required: true 
+//   },
+//   excelFileName: { 
+//     type: String, 
+//     required: true 
+//   },
+//   sheetName: { 
+//     type: String, 
+//     required: true 
+//   },
+//   rangeAddress: { 
+//     type: String, 
+//     required: true 
+//   },
+//   type: { 
+//     type: String, 
+//     enum: ["Table", "Chart"], 
+//     required: true 
+//   },
+//   dataSnapshot: { 
+//     type: mongoose.Schema.Types.Mixed, 
+//     required: true 
+//   },
+//   updatedAt: { 
+//     type: Date, 
+//     default: Date.now 
+//   }
+// });
+
+// module.exports = mongoose.model("Link", LinkSchema);
 const mongoose = require("mongoose");
 
 const LinkSchema = new mongoose.Schema({
@@ -5,6 +45,10 @@ const LinkSchema = new mongoose.Schema({
     type: String, 
     required: true, 
     unique: true 
+  },
+  componentName: { 
+    type: String, 
+    default: "" 
   },
   excelFileId: { 
     type: String, 
